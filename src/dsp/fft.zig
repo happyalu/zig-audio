6ab641707614dbcea13b209ab38a5ff4b1d3915c
Alok Parlikar <alok@cobaltspeech.com>
2021-06-03 08:54:18 +0530

    cleanup fft implementation

--- src/dsp/fft.zig
+++ src/dsp/fft.zig
@@ -7,12 +7,12 @@ pub const FFT = struct {
     length: usize,
     sin: []f32,
 
-    fn init(allocator: *std.mem.Allocator, length: usize) !Self {
-        const sin_table_size = length - (length / 4) + 1;
+    pub fn init(allocator: *std.mem.Allocator, max_length: usize) !Self {
+        const sin_table_size = max_length - (max_length / 4) + 1;
         var sin = try allocator.alloc(f32, sin_table_size);
         sin[0] = 0;
 
-        const a = std.math.pi / @intToFloat(f32, length) * 2;
+        const a = std.math.pi / @intToFloat(f32, max_length) * 2;
         var i: usize = 1;
         while (i < sin_table_size) : (i += 1) {
             sin[i] = std.math.sin(a * @intToFloat(f32, i));
@@ -20,17 +20,18 @@ pub const FFT = struct {
 
         return Self{
             .allocator = allocator,
-            .length = length,
+            .length = max_length,
             .sin = sin,
         };
     }
 
-    fn deinit(self: *Self) void {
+    pub fn deinit(self: *Self) void {
         self.allocator.free(self.sin);
     }
 
-    fn fftr(self: Self, real: []f32, imag: []f32) !void {
-        if (real.len != self.length or imag.len != self.length) return error.DataSizeMismatch;
+    pub fn fftr(self: Self, real: []f32, imag: []f32) !void {
+        if (real.len != imag.len) return error.DataSizeMismatch;
+        if (real.len > self.length or !std.math.isPowerOfTwo(real.len)) return error.InvalidSize;
 
         // we assume that imag is zero, but neither assert nor set zeros.
 
@@ -43,17 +44,17 @@ pub const FFT = struct {
         // real=(x0 x1 x2 x3 x4 x5 ... xN  xN+1 xN+2 ... x2N), imag=(0)
         // becomes
         // real=(x0 x2 x4 x6 ... x2N), imag=(1, 3, 5, 7, ... x2N-1)
-        var i: usize = 0;
 
-        while (i < self.length) : (i += 1) {
+        const m = real.len;
+
+        var i: usize = 0;
+        while (i < m) : (i += 1) {
             if (i % 2 == 0) {
                 real[i / 2] = real[i];
             } else {
                 imag[(i - 1) / 2] = real[i];
             }
         }
-
-        const m = self.length;
 
         // step 2: run FFT on the shuffled data.
         var x = real[0 .. m / 2];
@@ -62,51 +63,43 @@ pub const FFT = struct {
         try self.fft(x, y);
 
         // step 3: split operations
+        const sin_step = self.length / m;
         var sin_idx: usize = 0;
-        var cos_idx: usize = m / 4;
-
-        var p: usize = 0;
-        var q: usize = m;
-
-        real[p + m / 2] = real[p] - imag[p];
-        real[p] = real[p] + imag[p];
-        imag[p + m / 2] = 0;
-        imag[p] = 0;
-
+        var cos_idx: usize = self.length / 4;
         i = m / 2 - 1;
-        var j: i32 = @intCast(i32, m / 2) - 2;
         while (i > 0) : (i -= 1) {
-            p += 1;
-            sin_idx += 1;
-            cos_idx += 1;
-            const pj: usize = @intCast(usize, (@intCast(i32, p) + j));
-            const it = imag[p] + imag[pj];
-            const rt = real[p] - real[pj];
-            q -= 1;
-            real[q] = (real[p] + real[pj] + self.sin[cos_idx] * it - self.sin[sin_idx] * rt) * 0.5;
-            imag[q] = (imag[pj] - imag[p] + self.sin[sin_idx] * it + self.sin[cos_idx] * rt) * 0.5;
+            sin_idx += sin_step;
+            cos_idx += sin_step;
+            const sin = self.sin[sin_idx];
+            const cos = self.sin[cos_idx];
+            const tmp_imag = imag[m / 2 - i] + imag[i];
+            const tmp_real = real[m / 2 - i] - real[i];
 
-            j -= 2;
+            real[m / 2 + i] = (real[m / 2 - i] + real[i] + cos * tmp_imag - sin * tmp_real) * 0.5;
+            imag[m / 2 + i] = (imag[i] - imag[m / 2 - i] + sin * tmp_imag + cos * tmp_real) * 0.5;
         }
 
-        p = 1;
-        q = m;
+        real[m / 2] = real[0] - imag[0];
+        imag[m / 2] = 0;
+        real[0] = real[0] + imag[0];
+        imag[0] = 0;
 
-        i = m / 2;
-        while (i > 0) : (i -= 1) {
-            q -= 1;
-            real[p] = real[q];
-            imag[p] = -imag[q];
-            p += 1;
+        // Use complex conjugate properties to get the rest of the transform.
+
+        i = 1;
+        while (i < m / 2) : (i += 1) {
+            real[i] = real[m - i];
+            imag[i] = -imag[m - i];
         }
     }
 
-    fn fft(self: Self, x: []f32, y: []f32) !void {
-        if (x.len != y.len) return error.DataSizeMismatch;
+    pub fn fft(self: Self, real: []f32, imag: []f32) !void {
+        if (real.len != imag.len) return error.DataSizeMismatch;
+        if (real.len > self.length or !std.math.isPowerOfTwo(real.len)) return error.InvalidSize;
 
         // iterative, in-place radix-2 fft
-        var sin_step: usize = self.length / x.len;
-        var n: usize = x.len;
+        var sin_step: usize = self.length / real.len;
+        var n: usize = real.len;
         var m: usize = undefined;
 
         while (true) {
@@ -121,13 +114,13 @@ pub const FFT = struct {
             while (j < n) : (j += 1) {
                 var p = j;
                 var k: usize = m;
-                while (k <= x.len) : (k += m) {
-                    const t1 = x[p] - x[p + n];
-                    const t2 = y[p] - y[p + n];
-                    x[p] += x[p + n];
-                    y[p] += y[p + n];
-                    x[p + n] = self.sin[cos_idx] * t1 + self.sin[sin_idx] * t2;
-                    y[p + n] = self.sin[cos_idx] * t2 - self.sin[sin_idx] * t1;
+                while (k <= real.len) : (k += m) {
+                    const t1 = real[p] - real[p + n];
+                    const t2 = imag[p] - imag[p + n];
+                    real[p] += real[p + n];
+                    imag[p] += imag[p + n];
+                    real[p + n] = self.sin[cos_idx] * t1 + self.sin[sin_idx] * t2;
+                    imag[p + n] = self.sin[cos_idx] * t2 - self.sin[sin_idx] * t1;
                     p += m;
                 }
                 sin_idx += sin_step;
@@ -136,16 +129,16 @@ pub const FFT = struct {
             sin_step += sin_step;
         }
 
-        n = x.len;
+        n = real.len;
         var p: usize = 0;
         var k: usize = n / 2;
         while (k > 0) : (k -= 1) {
-            const t1 = x[p] - x[p + 1];
-            const t2 = y[p] - y[p + 1];
-            x[p] += x[p + 1];
-            y[p] += y[p + 1];
-            x[p + 1] = t1;
-            y[p + 1] = t2;
+            const t1 = real[p] - real[p + 1];
+            const t2 = imag[p] - imag[p + 1];
+            real[p] += real[p + 1];
+            imag[p] += imag[p + 1];
+            real[p + 1] = t1;
+            imag[p + 1] = t2;
             p += 2;
         }
 
@@ -154,24 +147,28 @@ pub const FFT = struct {
         const n_bits = std.math.log2_int(usize, n);
         const shiftr = @intCast(@TypeOf(n_bits), @bitSizeOf(usize) - 1) - n_bits + 1;
         var i: usize = 0;
-        while (i < n / 2) : (i += 1) {
+        while (i < n) : (i += 1) {
             var j: usize = @bitReverse(usize, i);
             j >>= shiftr;
 
-            const t1 = x[i];
-            const t2 = y[i];
+            if (i <= j) {
+                continue;
+            }
 
-            x[i] = x[j];
-            y[i] = y[j];
+            const t1 = real[i];
+            const t2 = imag[i];
 
-            x[j] = t1;
-            y[j] = t2;
+            real[i] = real[j];
+            imag[i] = imag[j];
+
+            real[j] = t1;
+            imag[j] = t2;
         }
     }
 };
 
 test "fft" {
-    var f = try FFT.init(std.testing.allocator, 16);
+    var f = try FFT.init(std.testing.allocator, 32);
     defer f.deinit();
 
     var real = [_]f32{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0, 0, 0, 0, 0 };
