d3260ca51de511f2fc670bad4e53c58762ea6c8f
Alok Parlikar <alok@cobaltspeech.com>
2021-06-08 16:49:58 +0530

    export audio.frame

--- audio.zig
+++ audio.zig
@@ -1,5 +1,8 @@
 pub const waveReaderFloat32 = @import("src/wav/wav.zig").waveReaderFloat32;
 pub const waveReaderPCM16 = @import("src/wav/wav.zig").waveReaderFloat16;
-pub const frameMaker = @import("src/dsp/frame.zig").frameMaker;
+
+pub const frame = @import("src/dsp/frame.zig");
+pub const frameMaker = frame.frameMaker;
+
 pub const FFT = @import("src/dsp/fft.zig").FFT;
 pub const mfccMaker = @import("src/dsp/melfreq.zig").mfccMaker;
